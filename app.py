c1c12363054eb455dddb8ba5e7324a8b39f598f2
Alejandro Cervera Loza <alejandrocerveraloza@gmail.com>
2026-02-16 19:42:09 +0100

    Fix port for Render

--- app.py
+++ app.py
@@ -73,4 +73,6 @@ def ask():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Render asigna un puerto automáticamente, esto permite capturarlo
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
